3c3b6e8869e37c322cd350a9573f2238b5d8f229
eriakbar3 <eakbar50@gmail.com>
2019-10-15 03:22:26 +0700

    add canvas dragdrop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
+import Modal from '@material-ui/core/Modal';
+import Fade from '@material-ui/core/Fade';
+import { makeStyles } from '@material-ui/core/styles';
+import {fabric} from 'fabric';
 import logo from './logo.svg';
+import golden from './goldencircle.png';
+import vpc from './vpc.png';
 import './App.css';
 import imgpointer from './cursor.svg';
 import txticon from './text.png';
@@ -8,25 +14,42 @@ import socketIOClient from 'socket.io-client';
 import axios from 'axios';
 const queryString = require('query-string');
 const socket = socketIOClient("localhost:4001");
+    const idgolden = "golden";
+    const idvpc = "vpc";
 let cur = {};
 
-const that = this;
+var fcanvas = new fabric.Canvas('canvas');
 //let canvas = document.getElementById("canvas");
 class App extends Component {
 
+  handleOpen = () => {
+    this.setState({
+      setOpen:true
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      setOpen:false
+    });
+  };
   constructor(props) {
     super(props);
-
     this.state = {
       styles : {display:"none"},
       classpoint : "hide",
       x: 0,
       y: 0,
+      j:0,
+      xobj : 100,
+      yobj:100,
       endpoint:"localhost:4001",
       color: 'white',
       pointers:{},
       c:{},
+       setOpen : false,
      };
+
   }
 
   send = () => {
@@ -52,52 +75,79 @@ class App extends Component {
  }
 
 
+ _addImagetoCanvas = (image) => {
+     var images = document.getElementById(image);
+     console.log(fcanvas);
+     var fcanvas = new fabric.Canvas('canvas');
+     var imgInstance = new fabric.Image(images,{
+       left: 100,
+       top: 100,
+     });
+     fcanvas.add(imgInstance);
+      console.log(image);
+      //ctx.drawImage(images, this.state.xobj, this.state.yobj);
+      this.setState({
+        setOpen:false,
+      });
+      console.log(image);
+ };
+
+
   componentDidMount(){
 
-    console.log(window.location.search);
+
+    const canvast = document.getElementById("canvas");
+    canvast.width = window.innerWidth;     // equals window dimension
+    canvast.height = window.innerHeight;
+    //console.log(window.location.search);
     const parsed = queryString.parse(window.location.search);
     var url = 'http://localhost:8000/api/member/'+parsed.id+'/'+parsed.userid;
-    console.log(url);
+
     axios.get(url)
     .then(response=> {
       // handle success
       const data = response.data;
       cur = data;
-      // this.setState(
-      //   {c:data}
-      // );
       const curs = cur.map(data=><i id={data.user_id} class="hidden">{data.name}</i>);
       ReactDOM.render(curs, document.getElementById("cursor"));
-      console.log(cur);
+      //console.log(cur);
     });
-    console.log(cur);
+
     socket.emit("join_project",parsed);
-
-  // socket.on("ready",function(project){
-  //   const users = array.map((user)=>
-  //     <i id={user.userid}></i>
-  //   );
-  //
-  // })
   const curso = [];
-  console.log(this.state.c);
+  //console.log(this.state.c);
     socket.on("moving",function(data){
-        //this.setState({ top: data.data.x, left: data.data.y });
-      // cursors.style.left = this.state.x;
-      // cursors.style.top = this.state.y;
-       // console.log(data);
-
           var i = data.project.userid;
           curso[i] = document.getElementById(data.project.userid);
           curso[i].setAttribute("class","mouse pointer  icon");
           curso[i].setAttribute("style","position:absolute;display:grid;top:"+data.data.y+"px"+";left:"+data.data.x+"px")
-
     });
 
   }
   render(){
     return (
     <div className="App">
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      className="modal"
+      open={this.state.setOpen}
+      onClose={this.handleClose}
+      closeAfterTransition
+    >
+      <Fade in={this.state.setOpen}>
+        <div className="paper">
+          <div class="templatelist ui medium bordered">
+            <img class="ui medium  image" src={golden} id="golden" />
+            <button class="ui primary button" onClick={this._addImagetoCanvas.bind(this, idgolden)}>add</button>
+          </div>
+          <div class="templatelist ui medium bordered">
+            <img class="ui medium  image" src={vpc} id="vpc"/>
+            <button class="ui primary button" onClick={this._addImagetoCanvas.bind(this, idvpc)}>add</button>
+          </div>
+        </div>
+      </Fade>
+    </Modal>
       <header className="App-header">
         <img src="http://catalist.space/assets/images/catalist_logo_full.png" className="App-logo" alt="logo" />
         <nav>
@@ -108,7 +158,7 @@ class App extends Component {
       <div class="stationary">
         <a title="Cursors"> <i class="mouse pointer large icon"></i> </a>
         <a title="Text"> <i class="font large icon"></i> </a>
-        <a title="Template"> <i class=" table large icon"></i> </a>
+        <a title="Template" onClick={this.handleOpen}> <i class=" table large icon"></i> </a>
         <a title="Sticky Note"> <i class="sticky note outline large icon"></i></a>
         <a title="Connection"> <i class="exchange large icon"></i></a>
         <a title="Pencil"> <i class="pencil alternate large icon"></i></a>
